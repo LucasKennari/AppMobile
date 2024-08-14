bf0cd9da3899030e2b07a531a65d4915051c733c
lucas cezar <lucas.oheisen@gmail.com>
2024-08-14 20:46:53 -0300

    header bar removed

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,17 +1,16 @@
-
-import HomeScreen from "../screens/homeScreen";
-import DetailsScreen from "../screens/detailsScreen";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-
+import HomeScreen from '../screens/homeScreen';
+import DetailsScreen from '../screens/detailsScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 export default function RootStack() {
-    return (
-        <Stack.Navigator initialRouteName="home">
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator
+      initialRouteName="home"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
 }
