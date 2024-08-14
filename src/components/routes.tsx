aafb192589d84d2bbc5ab03fa63f626c48dbb478
lucas cezar <lucas.oheisen@gmail.com>
2024-08-14 11:26:10 -0300

    create stack routes for navigate

---
+++ src/components/routes.tsx
@@ -0,0 +1,17 @@
+
+import HomeScreen from "./screens/homeScreen";
+import DetailsScreen from "./screens/detailsScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+
+const Stack = createNativeStackNavigator();
+
+export default function RootStack() {
+    return (
+        <Stack.Navigator initialRouteName="home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+    );
+}
