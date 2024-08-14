f8b1d000f6dfe293678369b3747f4f3d77d232af
lucas cezar <lucas.oheisen@gmail.com>
2024-08-14 20:44:45 -0300

    modification to the file where we create a button with standardized styling and create a style file for it

--- src/components/buttons/btnNavigation/style.tsx
+++ src/components/buttons/btnNavigation/style.tsx
@@ -1,10 +1,42 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
 
-const styles = StyleSheet.create( {
-    textColor: {
-        color: "blue"
-    }
-})
+    justifyContent: 'flex-end',
+    alignItems: 'center',
 
-export default styles 
+    marginBottom: 45,
+  },
+  btnHome: {
+    width: 281,
+    height: 90,
+
+    alignContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+
+    backgroundColor: '#0D7C85',
+
+    borderWidth: 4,
+    borderColor: '#EBFF6E',
+    borderRadius: 15,
+    borderStyle: 'solid',
+
+    elevation: 3,
+    shadowColor: '#000000',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 1,
+  },
+  textBtnHome: {
+
+    color: '#EBFF6E',
+    fontFamily: 'monospace',
+    fontWeight: "bold",
+    fontSize: 25,
+    lineHeight: 43.85,
+  },
+});
+
+export default styles;
