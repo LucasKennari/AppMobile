21c9ad9f06db8674bcb6363c116240c94e89f2e3
lucas cezar <lucas.oheisen@gmail.com>
2024-08-20 22:59:25 -0300

    Renaming changes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,12 @@
-import HomeScreen from '../screens/homeScreen';
 import DetailsScreen from '../screens/detailsScreen';
+import HomeScreen from '../screens/homeScreen/index';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
 const Stack = createNativeStackNavigator();
 
 export default function RootStack() {
   return (
     <Stack.Navigator
-      initialRouteName="home"
+      initialRouteName="Home"
       screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailsScreen} />
