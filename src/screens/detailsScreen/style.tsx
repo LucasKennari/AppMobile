b6f82c2bffa578e88249aa983a72008b4492360c
lucas cezar <lucas.oheisen@gmail.com>
2024-08-20 22:55:18 -0300

    Created the style file for the DetailsScreen

---
+++ src/screens/detailsScreen/style.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0F3A40',
+    paddingTop: 16,
+  },
+  search: {
+    // padding: 80,
+  },
+  tagContainer: {
+    // justifyContent: 'center',
+  },
+});
+export default styles;
