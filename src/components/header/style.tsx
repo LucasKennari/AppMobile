af12d36ebca5606b7eaa2150e72be080e8870266
lucas cezar <lucas.oheisen@gmail.com>
2024-08-20 23:04:22 -0300

    styling for header

---
+++ src/components/header/style.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    paddingTop: 40,
+  },
+  logo: {
+    width: '86%',
+    alignSelf: 'center',
+    paddingTop: 40,
+  },
+});
+
+export default styles;
