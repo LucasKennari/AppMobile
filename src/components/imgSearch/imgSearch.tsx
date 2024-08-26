fa9a43dc6c4fddc39eb0fed70f59790262c75d73
lucas cezar <lucas.oheisen@gmail.com>
2024-08-26 10:35:39 -0300

    refactoring code, searcInput component moved to details index

---
+++ src/components/imgSearch/imgSearch.tsx
@@ -0,0 +1,10 @@
+import {Image} from 'react-native';
+import searchIcon from '../../../android/app/src/main/res/drawable/searchIcon.png';
+
+export default function ImgSearch() {
+  return (
+    <>
+      <Image source={searchIcon} />
+    </>
+  );
+}
